50896c5beb5476084e85e2d427e1e07568a67b73
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-01 21:02:34 -0700

    SAD

--- listen.py
+++ listen.py
@@ -2,7 +2,7 @@ import serial
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 
 while True:
-    i = raw_input("Enter a char: ")
+    # i = raw_input("Enter a char: ")
     # i = input("Enter Char")
     # arduino.write(i)
     # arduino.write(i)
