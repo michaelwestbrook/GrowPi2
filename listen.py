2c0606327de12dcdbbf3a9ba839bce163cf2a142
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-01 20:40:17 -0700

    speak

--- listen.py
+++ listen.py
@@ -1,6 +1,7 @@
 import serial
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
-while True:
+while input = input():
+    arduino.write(input)
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
 		print data
