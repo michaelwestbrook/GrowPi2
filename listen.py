a393d1f42d4634cea61a8617f928fa750b76fd63
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-02 12:10:20 +0100

    foo

--- listen.py
+++ listen.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 import serial
 import os
+import threading 
 
+TIMEOUT = 3
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 timestr = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ".gpi"
 fileOut = open(timestr, "w+")
 print "Logging to: " + timestr
-while True:
+def timed():
+    arduino.write(32)
     data = arduino.readline()
     if data:
         out = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ":" + data
-        print(out)
+        print out
         fileOut.write(out)
         fileOut.flush()
         os.fsync(fileOut.fileno())
+    threading.Timer(TIMEOUT, timed).start()
+
+timed()
