dda816899656cada04bab3d1d2b0b18c8939daab
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-01 21:02:07 -0700

    asdf

--- listen.py
+++ listen.py
@@ -5,7 +5,7 @@ while True:
     i = raw_input("Enter a char: ")
     # i = input("Enter Char")
     # arduino.write(i)
-    arduino.write(i)
+    # arduino.write(i)
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
 		print data
