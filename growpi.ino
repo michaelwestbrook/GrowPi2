9be56d15196f6f1da6c220923f05b8b1624b081b
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-01 21:24:05 -0700

    loop

--- growpi.ino
+++ growpi.ino
@@ -214,18 +214,18 @@ void loop(void)
     byte nr = Serial.read();
     Serial.print("The following char was received: ");
     Serial.println(nr, DEC);
-    //simpleRead(); 
-    advancedRead();
-    // unifiedSensorAPIRead();
+    // //simpleRead(); 
+    // advancedRead();
+    // // unifiedSensorAPIRead();
 
-    // call sensors.requestTemperatures() to issue a global temperature 
-    // request to all devices on the bus 
-    /********************************************************************/
-    sensors.requestTemperatures(); // Send the command to get temperature readings 
-    /********************************************************************/
-    Serial.print("Temperature is: "); 
-    Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
-    // You can have more than one DS18B20 on the same bus.  
-    // 0 refers to the first IC on the wire 
+    // // call sensors.requestTemperatures() to issue a global temperature 
+    // // request to all devices on the bus 
+    // /********************************************************************/
+    // sensors.requestTemperatures(); // Send the command to get temperature readings 
+    // /********************************************************************/
+    // Serial.print("Temperature is: "); 
+    // Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+    // // You can have more than one DS18B20 on the same bus.  
+    // // 0 refers to the first IC on the wire 
   }
 }
