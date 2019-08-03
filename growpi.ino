6b2337cc4a875c21a120bea7d74a3f75529bd71d
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-03 05:36:22 +0100

    wip

--- growpi.ino
+++ growpi.ino
@@ -1,7 +1,3 @@
-/* TSL2591 Digital Light Sensor */
-/* Dynamic Range: 600M:1 */
-/* Maximum Lux: 88K */
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_TSL2591.h"
@@ -9,26 +5,25 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h>
 
-/********************************************************************/
-// Data wire is plugged into pin 2 on the Arduino 
-#define ONE_WIRE_BUS 2 
-/********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices  
-// (not just Maxim/Dallas temperature ICs) 
-OneWire oneWire(ONE_WIRE_BUS); 
-/********************************************************************/
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-/********************************************************************/ 
+// Temperature Sensor 
+#define TEMP_SENSOR_BUS 2 
+OneWire oneWire(TEMP_SENSOR_BUS); 
+DallasTemperature temperatureSensor(&oneWire);
 
-// Example for demonstrating the TSL2591 library - public domain!
-
-// connect SCL to analog 5
-// connect SDA to analog 4
-// connect Vin to 3.3-5V DC
-// connect GROUND to common ground
-
+// Lux Sensor
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+
+// Relays
+int RELAY_POWER = 5;
+int ONE = 6;
+int TWO = 7;
+int THREE = 8;
+int FOUR = 9;
+int FIVE = 10;
+int SIX = 11;
+int SEVEN = 12;
+int EIGHT = 13;
+int relayState = HIGH;
 
 
 /**************************************************************************/
@@ -36,7 +31,7 @@ Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the senso
     Configures the gain and integration time for the TSL2591
 */
 /**************************************************************************/
-void configureSensor(void)
+void configureLuxSensor(void)
 {
   // You can change the gain on the fly, to adapt to brighter/dimmer light situations
   //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
@@ -48,37 +43,59 @@ void configureSensor(void)
   tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
 }
 
+void configureRelays() {
+  pinMode(RELAY_POWER, OUTPUT);
+  digitalWrite(RELAY_POWER, relayState);
+  pinMode(ONE, OUTPUT);
+  pinMode(TWO, OUTPUT);
+  pinMode(THREE, OUTPUT);
+  pinMode(FOUR, OUTPUT);
+  pinMode(FIVE, OUTPUT);
+  pinMode(SIX, OUTPUT);
+  pinMode(SEVEN, OUTPUT);
+  pinMode(EIGHT, OUTPUT); 
+}
 
-/**************************************************************************/
-/*
-    Program entry point for the Arduino sketch
-*/
-/**************************************************************************/
-void setup(void) 
-{
+void setup(void){
   Serial.begin(9600);
-  sensors.begin(); 
+  temperatureSensor.begin(); 
+  
   if (!tsl.begin()) 
   {
     Serial.println("No sensor found ... check your wiring?");
     while (1);
   }
       
-  configureSensor();
+  configureLuxSensor();
+  configureRelays();
 }
 
-void loop(void) 
-{ 
-  if (Serial.available() > 0) {
-    int inByte = Serial.read();
-    // Serial.println(inByte);
-    uint32_t lum = tsl.getFullLuminosity();
-    uint16_t ir, full;
-    ir = lum >> 16;
-    full = lum & 0xFFFF;
-    float lux = tsl.calculateLux(full, ir);
-    sensors.requestTemperatures();
-    float temp = sensors.getTempCByIndex(0);
-    Serial.println("{\"IR\":" + String(ir) + ",\"Visible\":" + String(full - ir) + "\"Full\":" + String(full) + ",\"Temperature\":" + String(temp) + "}");
-  }
+void loop() {}
+unsigned char status = OFF;
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  int inByte = Serial.read();
+  status = status == ON ? OFF : ON;
+  // Serial.println(status);
+  digitalWrite(ONE, status);
+  digitalWrite(TWO, status);
+  digitalWrite(THREE, status);
+  digitalWrite(FOUR, status);
+  digitalWrite(FIVE, status);
+  digitalWrite(SIX, status);
+  digitalWrite(SEVEN, status);
+  digitalWrite(EIGHT, status);
+  Serial.println(inByte);
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  float lux = tsl.calculateLux(full, ir);
+  temperatureSensor.requestTemperatures();
+  float temp = temperatureSensor.getTempCByIndex(0);
+  // Serial.println("{\"IR\":" + String(ir) + ",\"Visible\":" + String(full - ir) + "\"Full\":" + String(full) + ",\"Temperature\":" + String(temp) + "}");
 }
--- listen.py
+++ listen.py
@@ -1,22 +1,24 @@
 from datetime import datetime
+import time
 import serial
 import os
 import threading 
 
-TIMEOUT = 3
+TIMEOUT = 5
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
 timestr = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ".gpi"
 fileOut = open(timestr, "w+")
 print "Logging to: " + timestr
 def timed():
-    arduino.write(32)
-    data = arduino.readline()
-    if data:
-        out = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ":" + data
-        print out
-        fileOut.write(out)
-        fileOut.flush()
-        os.fsync(fileOut.fileno())
-    threading.Timer(TIMEOUT, timed).start()
+    now = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+    arduino.write(100)
+    data = arduino.readline()[:-1]
+    out = now + ":" + data
+    print out
+    fileOut.write(out)
+    fileOut.flush()
+    os.fsync(fileOut.fileno())
 
-timed()
+while True:
+    timed()
+    time.sleep(TIMEOUT)
