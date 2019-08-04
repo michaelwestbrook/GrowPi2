28181c806b45d1faf79804addc1579fe5c9a91ce
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-04 21:30:40 +0100

    cycle

--- listen.py
+++ listen.py
@@ -4,10 +4,8 @@ import serial
 import os
 import threading
 
-TIMEOUT = 0.5
+TIMEOUT = 1
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
-# arduino.reset_input_buffer()
-# arduino.reset_output_buffer()
 timestr = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ".gpi"
 fileOut = open(timestr, "w+")
 print "Logging to: " + timestr
@@ -25,16 +23,16 @@ def readArduino():
     fileOut.flush()
     os.fsync(fileOut.fileno())
 
-
+relays = 0
 while True:
     try:
-        command = input("command: ")
-        sensors = input("Sensors: ")
-        relays = input("Relays: ")
+        command = 3
+        sensors = 6
         if command >= 0 and command < 256 and sensors >= 0 and sensors < 8 and relays >= 0 and relays < 256:
             arduino.write([command, sensors, relays])
             time.sleep(TIMEOUT)
             readArduino()
+            relays = (relays + 1) % 256
         else:
             print "Got unexpected commands"
     except (KeyboardInterrupt, SystemExit):
