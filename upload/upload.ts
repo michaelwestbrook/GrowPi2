c9836a6e6c4842de0c27e64cc9d341517e7cb209
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-10 21:05:33 +0100

    npm init

--- upload/upload.ts
+++ upload/upload.ts
@@ -1,7 +1,15 @@
-function greeter(person: String) {
-    return "Hello, " + person;
-}
+import SerialPort from 'serialport';
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
+// Read data that is available but keep the stream in "paused mode"
+port.on('readable', () => console.log('Data:', port.read()));
 
-let user = "Jane User";
+// Switches the port into "flowing mode"
+port.on('data', data => console.log('Data:', data));
+port.on('error', err => console.log('Error: ', err.message));
+port.write([3, 7, 255], err => {
+    if (err) {
+        console.error(err);
+    }
 
-document.body.textContent = greeter(user);
+    console.log('written')
+});
