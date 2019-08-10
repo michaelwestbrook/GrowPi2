fe8c5ad8988143677721b222aa8b60ff07edbca5
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-10 18:59:38 +0100

    foo

--- listen.py
+++ listen.py
@@ -4,9 +4,11 @@ import serial
 import os
 import threading
 
-TIMEOUT = 30
+TIMEOUT = 1
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
-timestr = datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ".gpi"
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+timestr = "logs/" + datetime.utcnow().strftime("%Y%m%d-%H%M%S") + ".gpi"
 fileOut = open(timestr, "w+")
 print "Logging to: " + timestr
 
@@ -14,28 +16,27 @@ print "Logging to: " + timestr
 def readArduino():
     now = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
     data = arduino.readline()[:-1]
-    while not data:
+    attempts = 0
+    while not data and attempts <= 100:
         data = arduino.readline()[:-1]
+        attempts = attempts + 1
 
-    out = now + ":" + data
-    print out
-    fileOut.writelines(out)
-    fileOut.flush()
-    os.fsync(fileOut.fileno())
+    if data :
+        out = now + ":" + data
+        print out
+        fileOut.writelines(out + "\n")
+        fileOut.flush()
+        os.fsync(fileOut.fileno())
 
 command = 3
-sensors = 6
+sensors = 7
 relays = 0
 while True:
     try:
-        if command >= 0 and command < 256 and sensors >= 0 and sensors < 8 and relays >= 0 and relays < 256:
-            arduino.write([command, sensors, relays])
-            time.sleep(1)
-            readArduino()
-            relays = (relays + 1) % 256
-            time.sleep(TIMEOUT)
-        else:
-            print "Got unexpected commands"
+        arduino.write([command, sensors, relays])
+        time.sleep(TIMEOUT)
+        readArduino()
+        relays = (relays + 1) % 256
     except (KeyboardInterrupt, SystemExit):
         print "\nexiting gracefully"
         arduino.write([2, 0, 0])
