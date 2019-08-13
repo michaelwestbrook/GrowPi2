ecfb14fa6ee20895c9ab7a165f36816fafc0261f
michaelwestbrook <michaelrwestbrook@gmail.com>
2019-08-13 05:57:00 +0100

    disable lint

--- upload/console-listener.ts
+++ upload/console-listener.ts
@@ -3,10 +3,12 @@ import GrowPiReading from "./growpi-reading";
 
 export class ConsoleListener implements IGrowPiListener {
   public readingReceived(readings: GrowPiReading[]) {
+    // tslint:disable-next-line: no-console
     console.log(JSON.stringify(readings, null, 2));
   }
 
   public error(error: Error) {
+    // tslint:disable-next-line: no-console
     console.error(error);
   }
 }
